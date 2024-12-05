5d64be7a99f0a0389676c9070c015fc51b1d1cf0
Asmita Das <dasasmi.79@gmail.com>
2024-12-05 21:14:25 +0530

    updated

--- app/routes/admin/product.route.js
+++ app/routes/admin/product.route.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const route_label = require("route-label");
+const namedRouter = route_label(router);
 const Auth = require("../../middleware/auth");
 const productController = require("../../modules/product/controller/product.controller");
 
@@ -8,3 +10,5 @@ namedRouter.all("/faq*", Auth.authenticate);
 namedRouter.post("product.add", '/product/add', productController.add);
 namedRouter.post("product.update", '/product/update', productController.update);
 namedRouter.get("product.delete", '/product/delete/:id', productController.delete);
+
+module.exports = router;
